5ea5883736df0dd49b336b1a03b6dfe5fc76ebe1
hudfgfk <116672298+hudfgfk@users.noreply.github.com>
2023-05-11 16:16:52 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,8 @@
+var primeiroNome="Marcus";
+var ultimoNome="Daniel";
+var a = 37;
+var b = 42;
+var fullName=primeiroNome.concat(ultimoNome);
+console.log(fullName);
+var soma=a+b;
+console.log(soma);
